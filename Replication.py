2604d25ed20dfb147edcb14ec3f7e1711cc18b8e
zjn0505 <zjn0505@hotmail.com>
2017-03-12 22:57:13 +0800

    Update for week 2

--- Replication.py
+++ Replication.py
@@ -54,6 +54,70 @@ def PatternMatching(Pattern, Genome):
             positions.append(i)
     return positions
 
+
+
+# Input:  Two strings p and q
+# Output: An integer value representing the Hamming Distance between p and q.
+def HammingDistance(p, q):
+    # your code here
+    count = 0
+    for i in range(0, len(p)):
+        if p[i] != q[i]:
+            count += 1
+    return count
+
+
+# Input:  A DNA string Genome
+# Output: A list containing all integers i minimizing Skew(Prefix_i(Text)) over all values of i (from 0 to |Genome|)
+def MinimumSkew(Genome):
+    positions = [] # output variable
+    # your code here
+    skew = Skew(Genome)
+    minValue = min(skew.values())
+    for i in range(0, len(skew)):
+        if skew[i] == minValue:
+            positions.append(i)
+    return positions
+
+# Input:  A String Genome
+# Output: Skew(Genome)
+# HINT:   This code should be taken from the last Code Challenge.
+def Skew(Genome):
+    skew = {} #initializing the dictionary
+    # your code here
+    skew[0] = 0
+    for i in range(0, len(Genome)):
+        if Genome[i] == "C":
+            skew[i + 1] = skew[i] - 1
+        elif Genome[i] == "G":
+            skew[i + 1] = skew[i] + 1
+        else:
+            skew[i + 1] = skew[i]
+    return skew
+
+# Input:  Strings Pattern and Text along with an integer d
+# Output: A list containing all starting positions where Pattern appears
+# as a substring of Text with at most d mismatches
+def ApproximatePatternMatching(Pattern, Text, d):
+    positions = [] # initializing list of positions
+    # your code here
+    for i in range(0, len(Text)-len(Pattern) + 1):
+        chop = Text[i: i + len(Pattern)]
+        if HammingDistance(Pattern, chop) <= d:
+            positions.append(i)
+    return positions
+
+# Input:  Strings Pattern and Text, and an integer d
+# Output: The number of times Pattern appears in Text with at most d mismatches
+def ApproximatePatternCount(Pattern, Text, d):
+    count = 0 # initialize count variable
+    # your code here
+    for i in range(0, len(Text)-len(Pattern) + 1):
+        chop = Text[i: i + len(Pattern)]
+        if HammingDistance(Pattern, chop) <= d:
+            count += 1
+    return count
+
 # Now set Text equal to the Vibrio cholerae oriC and k equal to 10
 
 oriC = "ATCAATGATCAACGTAAGCTTCTAAGCATGATCAAGGTGCTCACACAGTTTATCCACAACCTGAGTGGATGACATCAAGATAGGTCGTTGTATCTCCTTCCTCTCGTACTCTCATGACCACGGAAAGATGATCAAGAGAGGATGATTTCTTGGCCATATCGCAATGAATACTTGTGACTTGTGCTTCCAATTGACATCTTCAGCGCCATATTGCGCTGGCCAAGGTGACGGAGCGGGATTACGAAAGCATGATCATGGCTGTTGTTCTGTTTATCTTGTTTTGACTGAGACTTGTTAGGATAGACGGTTTTTCATCACTGACTAGCCAAAGCCTTACTCTGCCTGACATCGACCGTAAATTGATAATGAATTTACATGCTTCCGCGACGATTTACCTCTTGATCATCGATCCGATTGAAGATCTTCAATTGTTAATTCTCTTGCCTCGACTCATAGCCATGATGAGCTCTTGATCATGTTTCCTTAACCCTCTATTTTTTACGGAAGAATGATCAAGCTGCTGCTCTTGATCATCGTTTC"
